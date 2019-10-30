fa91e1910ce2cd466a94f4727134a8cb81aae518
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-10-29 19:52:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const submitData = (name, email) => {
+return  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+    .then(response => response.json())
+    .then(object => {
+      console.log(object)
+      let h1 = document.createElement('h1')
+      h1.innerHTML = "User id = " + object.id
+      document.querySelector('body').appendChild(h1)
+    })
+    .catch(err => document.querySelector('body').innerHTML = err)
+
+}
+
+submitData('moh', 'Moh@moh.com')
