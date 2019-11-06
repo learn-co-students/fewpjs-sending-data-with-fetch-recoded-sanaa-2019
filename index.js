32d7e57901223cb9aceb7f10eb6474dcbc2f2765
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-11-06 09:24:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(uName,uEmail)
+{
+  let dataObj = {name:uName,email:uEmail};
+  let configObj ={
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body: JSON.stringify(dataObj)
+  };
+  return fetch("http://localhost:3000/users",configObj)
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(obj){
+    document.body.innerHTML = document.body.innerHTML+obj.id+"";
+  })
+  .catch(function(e){
+    document.body.innerHTML = document.body.innerHTML+e.message+"";
+  })
+}
+
