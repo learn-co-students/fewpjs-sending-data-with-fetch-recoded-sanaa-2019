30b6b5e89ad7e23215ef891aa013993451d8aebc
Aisha Rashed <aisha1rashed@gmail.com>
2019-11-22 14:48:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+  return fetch("http://localhost:3000/users",{
+    method:"post",
+    headers:{
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail 
+    })
+  })
+  .then(function(responce){
+    return responce.json();
+  })
+  .then(function(obj){
+    document.body.innerHtml  = document.body.innerHtml+ obj.id + " ";
+  })
+  .catch(function(error){
+    document.body.innerHtml = error.message;
+  });
+}
