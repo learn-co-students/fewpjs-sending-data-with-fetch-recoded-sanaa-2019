571da0dd797208ce831bd671bdfc35b5ae6bad45
Sarah Alafif <sar1997yah@gmail.com>
2019-12-06 20:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(name,email){
+    return fetch('http://localhost:3000/users',{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify(
+            {
+                name:name,
+                email:email
+            }
+        )
+    }).then((jsonRes)=>jsonRes.json()).then((res)=>
+    {document.body.innerHTML = res.id})
+}
